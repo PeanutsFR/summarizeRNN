05d816c1631c58bef02725cee36a5c601187687c
PeanutsFR <peanuts.fr@gmail.com>
2015-02-11 15:38:59 +0100

    counts words frequency in a news article

--- load.py
+++ load.py
@@ -1,48 +1,31 @@
-import numpy as np
+#import numpy as np
 import os
+import operator
+from collections import Counter
 
-datasets_dir = '/media/datasets/'
+data_dir = '../data/'
 
-def one_hot(x,n):
-    if type(x) == list:
-        x = np.array(x)
-    x = x.flatten()
-    o_h = np.zeros((len(x),n))
-    o_h[np.arange(len(x)),x] = 1
-    return o_h
+# calculate words frequency in all articles [body + title]
+def create_freq_dict(data_file='2013-01.txt'):
 
-def mnist(ntrain=60000,ntest=10000,onehot=True):
-    data_dir = os.path.join(datasets_dir,'mnist/')
-    fd = open(os.path.join(data_dir,'train-images-idx3-ubyte'))
-    loaded = np.fromfile(file=fd,dtype=np.uint8)
-    trX = loaded[16:].reshape((60000,28*28)).astype(float)
+    freq_dict = Counter() # freq_dict["word"] = frequence
+    file = open(os.path.join(data_dir,data_file))
 
-    fd = open(os.path.join(data_dir,'train-labels-idx1-ubyte'))
-    loaded = np.fromfile(file=fd,dtype=np.uint8)
-    trY = loaded[8:].reshape((60000))
+    for line in file:
+        words = line.split()
+        # count words frequency of the title
+        #if "TITLE:" in words:
+        if "TITLE:" in words[:1]:
+            for word in words[1:]:
+                freq_dict[word] += 1
+        # count words frequency of the body
+        if "BODY:" in words:
+            for word in words[1:]:
+                freq_dict[word] += 1
 
-    fd = open(os.path.join(data_dir,'t10k-images-idx3-ubyte'))
-    loaded = np.fromfile(file=fd,dtype=np.uint8)
-    teX = loaded[16:].reshape((10000,28*28)).astype(float)
+    return freq_dict
 
-    fd = open(os.path.join(data_dir,'t10k-labels-idx1-ubyte'))
-    loaded = np.fromfile(file=fd,dtype=np.uint8)
-    teY = loaded[8:].reshape((10000))
-
-    trX = trX/255.
-    teX = teX/255.
-
-    trX = trX[:ntrain]
-    trY = trY[:ntrain]
-
-    teX = teX[:ntest]
-    teY = teY[:ntest]
-
-    if onehot:
-        trY = one_hot(trY, 10)
-        teY = one_hot(teY, 10)
-    else:
-        trY = np.asarray(trY)
-        teY = np.asarray(teY)
-
-    return trX,teX,trY,teY
+# displays the n-best words sorted by frequency
+def display_freq_dict(freq_dict={}, topwords=10):
+    for word,frequence in freq_dict.most_common(topwords):
+        print word, frequence
--- simple_rnn.py
+++ simple_rnn.py
@@ -1,28 +1,47 @@
-import theano
-from theano import tensor as T
-import numpy as np
+import load as ld
+#import theano
+#from theano import tensor as T
+#import numpy as np
 
-trX = np.linspace(-1, 1, 101)
-trY = 2 * trX + np.random.randn(*trX.shape) * 0.33
+frequency = {}
+frequency = ld.create_freq_dict()
+ld.display_freq_dict(frequency, 1000)
 
-X = T.scalar()
-Y = T.scalar()
 
-def model(X, w):
-    return X * w
 
-w = theano.shared(np.asarray(0., dtype=theano.config.floatX))
-y = model(X, w)
 
-cost = T.mean(T.sqr(y - Y))
-gradient = T.grad(cost=cost, wrt=w)
-updates = [[w, w - gradient * 0.01]]
 
-train = theano.function(inputs=[X, Y], outputs=cost, updates=updates, allow_input_downcast=True)
 
-for i in range(100):
-    for x, y in zip(trX, trY):
-        train(x, y)
 
-print w.get_value() #something around 2
+
+
+
+
+
+
+
+
+# trX = np.linspace(-1, 1, 101)
+# trY = 2 * trX + np.random.randn(*trX.shape) * 0.33
+
+# X = T.scalar()
+# Y = T.scalar()
+
+# def model(X, w):
+#     return X * w
+
+# w = theano.shared(np.asarray(0., dtype=theano.config.floatX))
+# y = model(X, w)
+
+# cost = T.mean(T.sqr(y - Y))
+# gradient = T.grad(cost=cost, wrt=w)
+# updates = [[w, w - gradient * 0.01]]
+
+# train = theano.function(inputs=[X, Y], outputs=cost, updates=updates, allow_input_downcast=True)
+
+# for i in range(100):
+#     for x, y in zip(trX, trY):
+#         train(x, y)
+
+# print w.get_value() #something around 2
 
